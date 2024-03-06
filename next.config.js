77c7521551aecd0fc3866a90abd1a7246cb3aaf9
ZealIT-Digital <159223066+ZealIT-Digital@users.noreply.github.com>
2024-03-06 11:36:13 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// const nextConfig = {};
 
-module.exports = nextConfig;
+// module.exports = nextConfig;
+
+module.exports = {[
+    {
+      name: "app-name",
+      script: "./node_modules/next/dist/bin/next",
+      args: "start -p " + (process.env.PORT || 3000),
+      watch: false,
+      autorestart: true,
+    },
+  ]};
