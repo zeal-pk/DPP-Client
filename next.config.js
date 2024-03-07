33d86a2d32366c72bd68341e7923a801b7a8e9c5
ZealIT-Digital <159223066+ZealIT-Digital@users.noreply.github.com>
2024-03-07 18:58:41 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  future: { webpack5: true },
+   webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
