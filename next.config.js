2007da5aaadced5df29ab5499183cd67adfab55b
ZealIT-Digital <zealitdigital@gmail.com>
2024-03-11 15:20:10 +0530

    next.config.js corrected

--- next.config.js
+++ next.config.js
@@ -1,16 +1,12 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-<<<<<<< HEAD
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback.fs = false;
     }
     return config;
   },
-=======
-   
->>>>>>> refs/remotes/origin/main
 };
 
 module.exports = nextConfig;
