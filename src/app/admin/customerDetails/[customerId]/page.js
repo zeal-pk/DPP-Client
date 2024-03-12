44d40c12bf17a5e05b17a03b0cdcc830714e274a
ZealIT-Digital <zealitdigital@gmail.com>
2024-03-12 18:16:53 +0530

    back button issue solve

--- src/app/admin/customerDetails/[customerId]/page.js
+++ src/app/admin/customerDetails/[customerId]/page.js
@@ -2,7 +2,7 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import NavBar from "@/components/navBar";
-import BackButton from "../../../components/backButton.js";
+import BackButton from "@components/backButton";
 import { inputData } from "../../../../database/data";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
